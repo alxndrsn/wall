356ec1b4be928d6404b70454f1e62c5aa9394812
Alex Anderson <anderson@medicmobile.org>
2015-08-04 15:01:37 +0200

    Add fix for limited number of results, and add counter to columns

--- js/wall.js
+++ js/wall.js
@@ -1,4 +1,4 @@
-Wall = window.Wall = function(containerSelecter, repo, columnTags) {
+Wall = window.Wall = function(placeholderSelecter, repo, columnTags) {
 	var ___start_vars___,
 	format = function(args) { args = args.length ? args : arguments;
 		return args.length === 1 ? args[0] : args[0].format.apply(
@@ -15,18 +15,24 @@ Wall = window.Wall = function(containerSelecter, repo, columnTags) {
 				},
 			});
 		},
-	}
+	},
 	withIssues = function(success) {
 		var path = 'repos/{0}/issues'.format(repo);
-		github.get(path, success, {
-			// TODO restrict to labels that we care about:
-			//labels: _.flatten(columnTags).join(','),
-			state: 'all',
+		// Do a separate request for each label we are interested in -
+		// the github API doesn't seem happy with labels with spaces
+		_.forEach(_.flatten(columnTags), function(label) {
+			github.get(path, success, {
+				state: 'all',
+				per_page: 100,
+				labels: label,
+			});
 		});
 	},
 	Table = function(columnTags) {
 		var ___start_vars___,
-		issues = [],
+		self = this,
+		cols = {},
+		element = $(sanchez.template('table')),
 		getPrimaryTag = function(issueOrTagGroup) {
 			if(_.isArray(issueOrTagGroup)) return issueOrTagGroup[0];
 			var issue = issueOrTagGroup;
@@ -43,58 +49,55 @@ Wall = window.Wall = function(containerSelecter, repo, columnTags) {
 
 		this.add = function(issue) {
 			log('Table.add() :: issue = #{0}', issue.number);
-			issues.push(issue);
+			var tag = getPrimaryTag(issue), count, counter;
+			if(!tag) {
+				return log('Ignoring issue: #{0} [{1}]', issue.number,
+						_.collect(issue.labels, function(it) { return it.name; }));
+			}
+
+			// TODO as we haven't implemented pagination, if we get
+			// to 100 results in a column then there's a good chance
+			// there are more than 100 that should be there
+			counter = $(element.find('thead th span.badge')[cols[tag][0].cellIndex]);
+			count = Number.parseInt(counter.text(), 10) + 1;
+			if(count >= 100) count = '100+';
+			counter.text(count);
+
+			cols[tag].append(sanchez.template('issue', {
+				url:issue.html_url, title:issue.title,
+				body:issue.body.chop(80),
+				'user.avatar_url':issue.user.avatar_url } ));
 		};
-		this.addAll = function(issues) { _.forEach(issues, this.add); };
-		this.getElement = function() {
-			var e = $(sanchez.template('table')),
-			    headRow = e.find('thead tr'),
-			    bodyRow = e.find('tbody tr'),
-			    cols = {};
-
-			// create columns
-			_.forEach(columnTags, function(tagGroup) {
-				var tag = getPrimaryTag(tagGroup),
-				    col = cols[tag] = $('<td class="{0}">'.
-						format(tagGroup.join(' ')));
-				headRow.append('<th class="col-md-{1}">{0}</td>'.format(
-					tagGroup.join(', '), 12/columnTags.length));
-				bodyRow.append(col);
-			});
-
-			// add issues
-			_.forEach(issues, function(issue) {
-				var tag = getPrimaryTag(issue);
-				if(!tag) {
-					return log('Ignoring issue: #{0} [{1}]', issue.number,
-							_.collect(issue.labels, function(it) { return it.name; }));
-				}
-				cols[tag].append(sanchez.template('issue', {
-					url:issue.html_url, title:issue.title,
-					body:issue.body.chop(80),
-					'user.avatar_url':issue.user.avatar_url } ));
-			});
-
-			return e;
-		};
+		this.addAll = function(issues) { _.forEach(issues, self.add); };
+		this.getElement = function() { return element; };
 
 		(function constructor() {
 			columnTags = _.collect(columnTags, function(tagGroup) {
 				return (typeof tagGroup === 'string')? [tagGroup]: tagGroup;
 			});
 			log('Constructing table for tags: ' + JSON.stringify(columnTags));
+
+			// create columns
+			var headRow = element.find('thead tr'),
+			    bodyRow = element.find('tbody tr');
+			_.forEach(columnTags, function(tagGroup) {
+				var tag = getPrimaryTag(tagGroup),
+				    col = cols[tag] = $('<td class="{0}">'.
+						format(tagGroup.join(' ')));
+				headRow.append(sanchez.template('column-header', {
+						heading: tagGroup.join(', '),
+						columns: 12/columnTags.length }));
+				bodyRow.append(col);
+			});
 		}());
 	},
 	___end_vars___ = true;
 
 	(function constructor() {
-		log('Initialising wall in container: ' + containerSelecter);
-		withIssues(function(issues) {
-			var table = new Table(columnTags),
-			    container = $(containerSelecter);
-			table.addAll(issues);
-			container.empty();
-			container.append(table.getElement());
-		});
+		log('Initialising wall in placeholder: ' + placeholderSelecter);
+		var table = new Table(columnTags),
+		    placeholder = $(placeholderSelecter);
+		placeholder.replaceWith(table.getElement());
+		withIssues(table.addAll);
 	}());
 };
